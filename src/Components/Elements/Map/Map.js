f1979b3d6380de58a724b5158ebb37844b4f0ea1
LaPatusek <ddrrusek@gmail.com>
2023-03-27 21:40:54 +0200

    v0.24

--- src/Components/Elements/Map/Map.js
+++ src/Components/Elements/Map/Map.js
@@ -1,28 +1,37 @@
 import GoogleMapReact from 'google-map-react';
+import { Location } from 'iconsax-react';
 import React from 'react';
 import styles from './Map.module.css';
 
-const MapPin = ({ text }) => <div>{text}</div>;
+const MapPin = () => (
+  <div className={styles.text}>
+    <Location variant='Bold' size='32' />
+    Niebylec 171
+  </div>
+);
 
 const Map = () => {
   const location = {
     center: {
-      lat: 37.42216,
-      lng: -122.08427,
+      lat: 49.85560328028798,
+      lng: 21.901875968601928,
     },
-    address: 'Niebylec 123',
-    zoom: 11,
+    zoom: 17,
   };
 
   return (
     <div className={styles.map}>
       <div className={styles['google-map']}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: '' }}
+          bootstrapURLKeys={{
+            key: '',
+            language: 'pl',
+          }}
           defaultCenter={location.center}
+          center={location.center}
           defaultZoom={location.zoom}
         >
-          <MapPin lat={59.955413} lng={30.337844} text={location.address} />
+          <MapPin lat={location.center.lat} lng={location.center.lng} />
         </GoogleMapReact>
       </div>
     </div>
